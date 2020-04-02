e3a28a4930b50c9fe0c574055c712b4857315c21
truongduyuit <uit.truongduy@gmail.com>
2020-04-02 20:53:22 +0700

    Version 1

---
+++ AirPort/Passenger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirPort
+{
+    public class Passenger
+    {
+        public string type { get; set; }
+        public string avatar { get; set; }
+        public Passenger()
+        {
+            Random rd = new Random();
+            int random = rd.Next(0, 101);
+
+            // random customer type
+            if (random < Constand.BUSINESS_COSTOMER_RANDOM_PERCENT)
+            {
+                type = Constand.CUSTOMER_TYPE_LIST[0];
+                avatar = @"\images\11.png";
+            }
+
+            else if (random >= Constand.BUSINESS_COSTOMER_RANDOM_PERCENT &&
+                        random < Constand.ONLINE_CUSTOMER_RANDOM_PERCENT + Constand.BUSINESS_COSTOMER_RANDOM_PERCENT)
+            {
+                type = Constand.CUSTOMER_TYPE_LIST[1];
+                avatar = @"\images\22.png";
+            }
+
+            else
+            {
+                type = Constand.CUSTOMER_TYPE_LIST[2];
+                avatar = @"\images\33.png";
+            }
+        }
+    }
+}
